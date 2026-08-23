a6923fc8c19c801e452dfe67752a8bb04e9e37af
Atharv Shukla <atharvshukla832@gmail.com>
2026-08-23 20:48:36 +0530

    Solve softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -3,16 +3,13 @@ import numpy as np
 def softmax(x: list) -> np.ndarray:
     """Return stable softmax probabilities with the same shape as x."""
     x = np.asarray(x)
-
     if x.ndim == 1:
         x_max = np.max(x)
-        exp_x = np.exp(x - x_max)
+        exp_x = np.exp(x-x_max)
         return exp_x / np.sum(exp_x)
-
     elif x.ndim == 2:
-        x_max = np.max(x, axis=1, keepdims=True)
-        exp_x = np.exp(x - x_max)
-        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
-
+        x_max = np.max(x, axis = 1, keepdims = True)
+        exp_x = np.exp(x-x_max)
+        return  exp_x/np.sum(exp_x, axis = 1, keepdims = True)
     else:
-        raise ValueError("Input must be a 1D or 2D array")
+        raise ValueError("x should be of 1D and 2D")
